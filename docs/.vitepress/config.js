ce9c380fd657c1e9fa1e2cee6c69bc9798ecfd6a
luis-espinoza <luis@stakeordie.com>
2021-06-21 14:55:12 -0700

    Update header links

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "Don't try to get off the ground without it",
   themeConfig: {
     nav: [
-      { text: "Read the docs", link: "https://docs.griptapejs.com/docs/" },
+      { text: "Read the docs", link: "https://docs.griptapejs.com" },
       { text: "Github", link: "https://github.com/stakeordie/griptape.js" },
       { text: "Secret Network", link: "https://scrt.network/" },
     ]
