98a883fcda221e12fe07a59d94878e0c07838e0c
luis-espinoza <luis@stakeordie.com>
2021-06-21 15:26:22 -0700

    Add favicon

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,14 @@
 module.exports = {
   title: "griptape.js",
   description: "Don't try to get off the ground without it",
+  head: [
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon-griptape.png' }]
+  ],
   themeConfig: {
     nav: [
       { text: "Read the docs", link: "https://docs.griptapejs.com" },
       { text: "Github", link: "https://github.com/stakeordie/griptape.js" },
       { text: "Secret Network", link: "https://scrt.network/" },
     ]
-  },
+  }
 };
